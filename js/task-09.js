936160aa92c2b9fd9a2e5f426a6b6931036d95e3
Olga Boiko <Itnumber1@ukr.net>
2023-04-25 22:34:07 +0300

    +hw8-9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const spanColor=document.querySelector(".color")
+const btnColor=document.querySelector(".change-color")
+const body=document.querySelector("body")
+
+btnColor.addEventListener('click', ()=>{
+body.style.backgroundColor = `${getRandomHexColor()}`;
+spanColor.textContent=getRandomHexColor();
+})
